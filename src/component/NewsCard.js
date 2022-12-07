6b56d12027213b4ea89f6ab49a7015fb60547f9a
Anmol Singh Bhatia <anmolsinghbhatia1001@gmail.com>
2022-12-08 00:24:12 +0530

    bg and hover Added

--- src/component/NewsCard.js
+++ src/component/NewsCard.js
@@ -3,7 +3,20 @@ import React from 'react'
 
 const NewsCard = (props) => {
   return (
-    <Card elevation={4} sx={{height: '200px', display: 'flex', flexDirection : 'column' ,  justifyContent: 'space-between' , padding: '8px'}} >
+    <Card  elevation={4} 
+    sx={{
+        height: '200px', 
+        display: 'flex', 
+        flexDirection : 'column' ,  
+        justifyContent: 'space-between' , 
+        padding: '8px', 
+        borderRadius : 3,
+        transition : '0.6s',
+        backgroundSize : '200%',
+        backgroundImage: 'linear-gradient(45deg, #FFFFFF, #6DD5FA)',
+        "&:hover" : {
+            backgroundImage: 'linear-gradient(45deg, #FFFFFF, #2980B9)',
+            backgroundPosition : 'right',}}} >
         <CardContent >
             <Typography sx={{typography: { xs: 'h6' , sm: 'h5'} }} component='div' gutterBottom>
                 {props.title}
