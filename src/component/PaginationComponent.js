e7eff700423c24a34055c03e0178eb7b9756880b
Anmol Singh Bhatia <anmolsinghbhatia1001@gmail.com>
2022-12-08 01:48:43 +0530

    Pagination Responsivness Added

--- src/component/PaginationComponent.js
+++ src/component/PaginationComponent.js
@@ -2,13 +2,20 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import { AppData } from '../context';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const PaginationComponent = () => {
   const {nbPages , page, handlePageChange} = React.useContext(AppData) 
+
+  const theme = useTheme()
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+  
   return (
     <Stack my={2} spacing={2} direction='row' justifyContent='center'>
       <Pagination count={nbPages} page={page + 1} variant='outlined' shape='rounded' 
-      onChange={(e,v)=> handlePageChange(e, (v-1))} />
+      onChange={(e,v)=> handlePageChange(e, (v-1))}  
+      size={mobile ? 'small' : 'medium'}
+      />
     </Stack>
   )
 }
